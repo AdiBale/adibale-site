341ab552fb888341f81d6ea967a5347f86f06da7
AdiBale <139584516+AdiBale@users.noreply.github.com>
2024-04-12 10:09:58 -0400

    Update writing.tsx

--- src/entries/writing.tsx
+++ src/entries/writing.tsx
@@ -105,7 +105,10 @@ ${image(img111323.default, 730, 675)}
 
 export const post041224 = `
 <br>
+(April 12, 2024)
+<br>
 Hello hello,
+<br>
 after 2 months in Singapore I can say couple things in Chinese: "you stinky"(nî chòu chòu) "how much?"(duö shâo) and "Oh no, that's not my tea"(Aï yà! zhe bü shì wô de chá!). I made these small collages out of my chinese writing practice notes, my "Roaring Spring" pink notepad paper, and random other materials like hong bao envelope, receipts from Taiwan, and different rice papers. 
 <br>
 ${image(img041224.default, 100, 100)}
